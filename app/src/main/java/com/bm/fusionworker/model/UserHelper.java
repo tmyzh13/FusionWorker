b1912f7345da208d556baaf7cc33cc7db4187686
你的åyizh <356589990@qq.com>
2018-05-05 00:02:38 +0800

    Initial commit

---
+++ app/src/main/java/com/bm/fusionworker/model/UserHelper.java
@@ -0,0 +1,35 @@
+package com.bm.fusionworker.model;
+
+
+import com.bm.fusionworker.model.beans.UserBean;
+import com.corelibs.utils.PreferencesHelper;
+
+
+public class UserHelper {
+    public static void saveUser(UserBean user) {
+        PreferencesHelper.saveData(user);
+    }
+
+    public static void saveUserInfo(UserBean userInfoBean) {
+        PreferencesHelper.saveData(userInfoBean);
+    }
+
+    public static UserBean getSavedUser() {
+        return PreferencesHelper.getData(UserBean.class);
+    }
+
+    public static long getUserId() {
+        return getSavedUser().id;
+    }
+    public static void clearUserInfo(Class user){
+        PreferencesHelper.remove(user);
+    }
+
+
+
+
+
+    public static  void clearInfo(String key){
+        PreferencesHelper.remove(key);
+    }
+}
